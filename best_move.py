8f13e631bbf715187494320d5b0fcfafae89dc4b
PaulJeFi <paul.jeromefilio@gmail.com>
2022-05-04 23:48:22 +0200

    review eval and search

--- best_move.py
+++ best_move.py
@@ -4,45 +4,126 @@ import search
 from move_generation import get_ordered_moves
 from tablebase_online import tablebase
 
+import time
+import evaluation
+
+def iterative_deepening(board: chess.Board, depth: int) -> tuple :
+
+    # Temps au debut
+    start_time = time.time()
+    curr_depth = 0
+    nodes = 0
+
+    # Move ordering avant la recherche
+    tmp_moves = search.get_ordered_moves(board)
+
+    # Premier meilleur coup (depth 0) :
+    b_move = tmp_moves[0]
+    board.push(b_move)
+    b_score = evaluation.evaluate(board)
+    scaled_score = evaluation.scale_to_white_view(board, b_score)
+    board.pop()
+
+    # uci report
+    print(f'info depth {curr_depth} score cp {int(scaled_score)} nodes {0} nps 0 time {int((time.time()-start_time) * 1000)} pv {str(b_move)}')
+
+    if depth < 1 :
+        return b_move, scaled_score
+    
+    last_time = time.time()
+
+    # Initialisation précise du move ordering : depth 1
+    moves = []
+    curr_depth += 1
+    for move in tmp_moves :
+        board.push(move)
+        searcher = search.Search(board, curr_depth-1)
+        tmp_value = searcher.pvSearch(depth=searcher.depth)
+        board.pop()
+        
+        nodes += searcher.nodes
+        moves.append((move, tmp_value))
+    
+    # Ordonner les coups :
+    moves = sorted(moves, key=lambda x: x[1])
+
+    # Retirer les doublons
+    old_moves = moves
+    moves = []
+    tmp_moves = []
+    for move, value in old_moves :
+        if not (move in tmp_moves) :
+            moves.append((move, value))
+            tmp_moves.append(move)
+    
+    # Extraire le meilleur coup et son évaluation
+    b_move = moves[0][0]
+    board.push(b_move)
+    scaled_score = evaluation.scale_to_white_view(board, moves[0][1])
+    board.pop()
+
+    # uci report
+    for ind, (move, value) in enumerate(moves) :
+        print(f'info depth {curr_depth} currmove {str(move)} currmovenumber {ind+1}') 
+    print(f'info depth {curr_depth} score cp {int(scaled_score)} nodes {nodes} nps {int(nodes/(time.time()-last_time))} time {int((time.time()-last_time) * 1000)} pv {str(b_move)}')
+    last_time = time.time()
+
+    while curr_depth < depth :
+        curr_depth += 1
+
+        old_moves = moves
+        moves = []
+        new_nodes = 0
+        for move, forget_this in old_moves :
+            board.push(move)
+            searcher = search.Search(board, curr_depth-1)
+            tmp_value = searcher.pvSearch(depth=searcher.depth)
+            board.pop()
+            
+            nodes += searcher.nodes
+            new_nodes += searcher.nodes
+            moves.append((move, tmp_value))
+        
+        # Ordonner les coups :
+        moves = sorted(moves, key=lambda x: x[1])
+
+        # Retirer les doublons
+        old_moves = moves
+        moves = []
+        tmp_moves = []
+        for move, value in old_moves :
+            if not (move in tmp_moves) :
+                moves.append((move, value))
+                tmp_moves.append(move)
+        
+        # Extraire le meilleur coup et son évaluation
+        b_move = moves[0][0]
+        board.push(b_move)
+        scaled_score = evaluation.scale_to_white_view(board, moves[0][1])
+        board.pop()
+
+        # uci report
+        for ind, (move, value) in enumerate(moves) :
+            print(f'info depth {curr_depth} currmove {str(move)} currmovenumber {ind+1}') 
+        print(f'info depth {curr_depth} score cp {int(scaled_score)} nodes {nodes} nps {int(new_nodes/(time.time()-last_time))} time {int((time.time()-last_time)) * 1000} pv {str(b_move)}')
+        last_time = time.time()
+
+    return b_move, scaled_score
+
 def best_move(board, timeleft=None, depth=2) :
     '''Détermine le meilleur coup.'''
     value = 0
     try :
         move = stockfish_book.move_from_book(board, stockfish_book.book)
         if move != None :
-            #print("Book : {}".format(move))
             return move, 0
     except Exception :
         pass
     move = tablebase(board)
     if move != None :
         return move, 0
-    else :
-        #print("On commence à chercher le meilleur coup.")
-        if type(timeleft) == int :
-            time = timeleft/1000
-            depth = 2
-            if time > 18*60 :
-                depth = 2
-            elif time > 30 :
-                depth = 2
-            elif time > 3 :
-                depth = 1
-            elif time <= 5 :
-                depth = 0
-                #print("Mode survie activé.")
-                #print("Aléatoire :", move)
-        else :
-            depth = depth
-            #print("Erreur au niveau du temps, depth : {}".format(depth))
-        if depth == 0 :
-            move = get_ordered_moves(board)[0]
-            value = 0
-        else :
-            move, value = search.search(board, depth)
-        #print("On va faire", move)
-        #print("Eval :", value)
-        return str(move), value
+
+    return iterative_deepening(board, 4)
 
 def game_is_finished(pos) :
     '''Détermine si la partie est finie.'''
