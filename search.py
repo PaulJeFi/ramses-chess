953604e93dc3ce320a03c7535cce92606562545c
PaulJeFi <paul.jeromefilio@gmail.com>
2022-05-04 23:42:22 +0200

    Revert "review search and eval"
    This reverts commit 99322f65e17dc874aaf3098352ab922f700cf5f0.

--- search.py
+++ search.py
@@ -1,179 +1,226 @@
 import chess
-from evaluation import mateValue, evaluate
+import eval
+from move_generation import get_ordered_moves, move_value
+import transposition
+import time
+from threading import Thread
+from copy import deepcopy
 
-mvv_lva = [
-        0,      0,   0,   0,   0,   0,   0,    0,   0,   0,   0,   0,   0,
 
-        0,    105, 205, 305, 405, 505, 605,  105, 205, 305, 405, 505, 605,
-        0,    104, 204, 304, 404, 504, 604,  104, 204, 304, 404, 504, 604,
-        0,    103, 203, 303, 403, 503, 603,  103, 203, 303, 403, 503, 603,
-        0,    102, 202, 302, 402, 502, 602,  102, 202, 302, 402, 502, 602,
-        0,    101, 201, 301, 401, 501, 601,  101, 201, 301, 401, 501, 601,
-        0,    100, 200, 300, 400, 500, 600,  100, 200, 300, 400, 500, 600,
+def quiesce(pos, alpha, beta, depth=4) :
+    '''Effecture une recherche de quiescence.'''
+    stand_pat = eval.evaluation_turn(pos)
+    #return stand_pat # Uncommenter cette ligne pour un moteur moins performant mais plus rapide.
+    if stand_pat >= beta :
+        return beta
+    if alpha < stand_pat:
+        alpha = stand_pat
+    if depth >= 0 :
+        for move in get_ordered_moves(pos) :
+            if pos.is_capture(move) :
+                pos.push(move)        
+                score = -quiesce(pos, -beta, -alpha, depth-1)
+                pos.pop()
+                if score >= beta :
+                    return beta
+                if score > alpha :
+                    alpha = score  
+    return alpha
 
-        0,    105, 205, 305, 405, 505, 605,  105, 205, 305, 405, 505, 605,
-        0,    104, 204, 304, 404, 504, 604,  104, 204, 304, 404, 504, 604,
-        0,    103, 203, 303, 403, 503, 603,  103, 203, 303, 403, 503, 603,
-        0,    102, 202, 302, 402, 502, 602,  102, 202, 302, 402, 502, 602,
-        0,    101, 201, 301, 401, 501, 601,  101, 201, 301, 401, 501, 601,
-        0,    100, 200, 300, 400, 500, 600,  100, 200, 300, 400, 500, 600
-    ]
-
-def get_ordered_moves(board: chess.Board, captures_only: bool=False) -> list :
-    '''Move orderings'''
-    
-    # Étape 1 : trier les captures selon MVV LVA
-    capture_moves = board.generate_legal_captures()
-
-    move_list = []
-    for move in capture_moves :
-        try :
-            move_list.append({
-                    'move': move,
-                    'cp': mvv_lva[
-                                board.piece_at(move.from_square).piece_type * 13 +
-                                board.piece_at(move.to_square).piece_type
-                            ]
-                })
-        except AttributeError : # cas d'une prise en passant, la pièce capturée
-             move_list.append({ # n'est pas sur la case d'arrivée
-                    'move': move,
-                    'cp': mvv_lva[
-                                board.piece_at(move.from_square).piece_type * 13 +
-                                chess.PAWN
-                            ]
-                })
-        
-    move_list = sorted(move_list, key=lambda k: k['cp'], reverse=True)
-
-    # Étape 2 : ne retourner que les captures si quiecsence :
-    if captures_only :
-        return [move['move'] for move in move_list]
-
-    # Étape 3 : savoir à partir d'où les captures ne sont pas bonnes (<= 150 cp)
-    # pour insérer avant les autres coups :
-    good_index = 0
-    if len(move_list) != 0 :
-        if not (True in move_list) :
-            good_index = len(move_list) - 1
+# https://www.chessprogramming.org/Principal_Variation_Search
+def pvSearch(board, alpha, beta, depth) :
+    '''PrincipaL Variation Search'''
+    if (depth == 0) :
+        return quiesce(board, alpha, beta)
+    bSearchPv = True
+    for move in get_ordered_moves(board) :
+        board.push(move)
+        if bSearchPv :
+            score = -pvSearch(board, -beta, -alpha, depth-1)
         else :
-            good_index = [(move['cp'] <= 150) for move in move_list].index(True)
+            score = -zwSearch(board, -alpha, depth - 1)
+            if (score > alpha) : # in fail-soft ... && score < beta ) is common
+                score = -pvSearch(board, -beta, -alpha, depth-1) # re-search
+        board.pop()
+        if (score >= beta) :
+            return beta   # fail-hard beta-cutoff
+        if (score > alpha) :
+            alpha = score # alpha acts like max in MiniMax
+            bSearchPv = False   # *1)
+    return alpha
 
-    # Étape 4 : insertion des autres coups
-    castle = board.generate_castling_moves()
-    move_list = [move['move'] for move in move_list]
-    for move in board.legal_moves :
-        if not (move in capture_moves) :
-            # Si le coup est une promotion, il est probablement bon
-            if move.promotion != None :
-                move_list.insert(0, move)
-            # Si le cou met en échec, il est aussi sans doute bon.
-            elif board.gives_check(move) :
-                move_list.insert(0, move)
-            # Si le coup est un roque, il ne doit pas être mauvais
-            elif move in castle :
-                move_list.insert(0, move)
-            else :
-                move_list.insert(good_index, move)
-            good_index += 1 # Update du good index (puisqu'on ajoute avant) 
-    
-    return move_list
-
-
-class Search :
-
-    def __init__(self, board=chess.Board, depth=3) -> None :
-        self.board = board
-        self.depth = depth
-        self.nodes = 0
-        self.base_number_of_moves = len(self.board.move_stack) # pour distance to mate dans quiesce
-    
-    def quiesce(self, alpha: float=-mateValue, beta: float=mateValue) -> float :
-        '''Effectue une recherche de quiescence'''
-
-        stand_pat = evaluate(self.board)
-        moves = get_ordered_moves(self.board, True)
-
-        # Fin de ligne
-        if len(moves) == 0 :
-            self.nodes += 1
-            if not bool(self.board.legal_moves) :
-                if self.board.is_checkmate() :
-                    return -mateValue + (self.base_number_of_moves - len(self.board.move_stack))
-                return 0
-
-        if  stand_pat >= beta :
-            return beta
-        if alpha < stand_pat :
-            alpha = stand_pat
-
-        for move in moves :
-            self.board.push(move)
-            score = -self.quiesce(-beta, -alpha)
-            self.board.pop()
-
-            if score >= beta :
-                return beta
-            if score > alpha :
-                alpha = score
-
-        return alpha
-
-    def zwSearch(self, beta: float, depth: int) -> float :
-        '''fail-hard zero window search, returns either beta-1 or beta'''
-        # alpha == beta - 1
-        # this is either a cut- or all-node
-
-        moves = self.board.legal_moves
-
-        # Fin de ligne
-        if not bool(moves) :
-            self.nodes += 1
-            if self.board.is_checkmate() :
-                return -mateValue + (self.depth - depth) # pour distance to mate
-            return 0
-
-        if depth == 0 :
-            return self.quiesce(beta-1, beta)
-
-        for move in moves :
-            self.board.push(move)
-            score = -self.zwSearch(1-beta, depth - 1)
-            self.board.pop()
-            if score >= beta :
-                return beta # fail-hard beta-cutoff
-
-        return beta-1 # fail-hard, return alpha
+# fail-hard zero window search, returns either beta-1 or beta
+def zwSearch(board, beta, depth) :
+    '''Zero Window Search'''
+    # alpha == beta - 1
+    # this is either a cut- or all-node
+    if (depth == 0) :
+        return quiesce(board, beta-1, beta)
+    for move in get_ordered_moves(board)  :
+        board.push(move)
+        score = -zwSearch(board, 1-beta, depth-1)
+        board.pop()
+        if (score >= beta) :
+            return beta   # fail-hard beta-cutoff
+    return beta-1 # fail-hard, return alpha
 
 
-    def pvSearch(self, alpha: float=-mateValue, beta: float=mateValue, depth=3) -> float :
+def PVS_root(depth: int, board: chess.Board, moves=None, t0=0) -> chess.Move:
+    '''Retourne le meilleur coup accompagné de son évaluation.'''
+    maximize = board.turn == chess.WHITE
+    best_move = -float("inf")
+    if not maximize:
+        best_move = float("inf")
 
-        moves = self.board.legal_moves
+    if moves == None :
+        moves = get_ordered_moves(board)
+    best_move_found = moves[0]
+    nodes = 0
+    if t0 == 0 :
+        t0 = time.time()
 
-        # Fin de ligne
-        if not bool(moves) :
-            self.nodes += 1
-            if self.board.is_checkmate() :
-                return -mateValue + (self.depth - depth) # pour distance to mate
-            return 0
+    for move in moves :
+        board.push(move)
+        if board.can_claim_draw():
+            value = 0.0
+        else :
+            value = transposition.find_eval(board, depth)
+            if value == None :
+                value = pvSearch(board, -float("inf"), float("inf"), depth-1)
+                if board.turn == chess.WHITE :
+                    pass
+                else :
+                    value = -value
+                if not maximize :
+                    evalu = -value
+                else :
+                    evalu = value
+                transposition.add_eval(board, depth, evalu)
+            elif not maximize :
+                value = -value
+        board.pop()
+        if maximize and value >= best_move :
+            best_move = value
+            best_move_found = move
+        elif not maximize and value <= best_move :
+            best_move = value
+            best_move_found = move
+        nodes += 1
+        print(f'info depth {depth} score cp {round(arround(best_move)*100)} nodes {nodes} time {round(1000 * (time.time() - t0))} pv {str(best_move_found)}')
+        # Pour l'instant, les nodes ne sont pas des vrais nodes.
 
-        if depth == 0 :
-            return self.quiesce(alpha, beta)
+    return best_move_found, best_move
 
-        bSearchPv = True
-        for move in moves :
-            self.board.push(move)
-            if bSearchPv :
-                score = -self.pvSearch(-beta, -alpha, depth - 1)
-            else :
-                score = -self.zwSearch(-alpha, depth - 1)
-                if ( score > alpha ) : # in fail-soft ... && score < beta ) is common
-                    score = -self.pvSearch(-beta, -alpha, depth - 1) # re-search
-            self.board.pop()
-            if  score >= beta :
-                return beta # fail-hard beta-cutoff
-            if score > alpha :
-                alpha = score # alpha acts like max in MiniMax
-                bSearchPv = False   # *1)
+def iterative_deepening(board, Depth) :
+    depth = 1
+    t0 = time.time()
+    moves = get_ordered_moves(board)
+    while depth != Depth :
+        moves = move_ordering(board, depth, t0, moves)
+        moves = moves[0:3]
+        depth += 1
+    return PVS_root(Depth, board, moves, t0)
+
+def move_ordering(board, depth, t0=0, moves=None) :
+    '''Pour les tests. Retourne les mouvements dans l'ordre par rapport à une depth données.'''
+    if t0 == 0 :
+        t0 = time.time()
+    if moves == None :
+        moves = get_ordered_moves(board)
+    if board.turn == chess.WHITE :
+        best_value = -float('inf')
+    else :
+        best_value = float('inf')
+    best_move = moves[0]
+    best_moves_values = []
+    best_moves = []
+    nodes = 0
+    for move in moves :
+        board.push(move)
+
+        if board.can_claim_draw():
+            value = 0.0
         
-        return alpha
+        else :
+            value = transposition.find_eval(board, depth)
+            if value == None :
+                value = pvSearch(board, -float("inf"), float("inf"), depth-1)
+                if board.turn == chess.WHITE :
+                    pass
+                else :
+                    value = -value
+                transposition.add_eval(board, depth, value)
+            else :
+                pass
+            best_moves.append(move)
+            best_moves_values.append(value)
+        nodes += 1
+        print(f'info depth {depth} score cp {round(arround(best_value)*100)} nodes {nodes} time {round(1000 * (time.time() - t0))} pv {str(best_move)}')
+        board.pop()
+        if board.turn == chess.WHITE :
+            if value > best_value :
+                    best_value = value
+                    best_move = move
+        else :
+            if value < best_value :
+                    best_value = value
+                    best_move = move
+
+    #print(f'\n{[round(value) for value in best_moves_values]}\n{[str(move) for move in best_moves]}\n')
+    return sorted(best_moves, key=lambda m: best_moves_values[best_moves.index(m)], reverse=board.turn)
+    
+
+def arround(value) :
+    '''Permet de retourner la valeur de mat numériquement.'''
+    if value == float('inf') :
+        return 10000000000
+    elif value == -float('inf') :
+        return -10000000000
+    else :
+        return value
+
+def search(board, depth) :
+    # return thread(board, depth) # with threads
+    '''On part de l'hypothèse que le meilleur coup en profondeur n se retrouve
+    dans les trois meilleurs coups en profondeur n. Cette hypothèse est vraie
+    63 % du temps (sans quiescence) et divise le temps de recherche par 11
+    si on ne prend que les trois meilleurs coups.'''
+    t0 = time.time()
+    conj = move_ordering(board, depth-1, t0)
+    if len(conj) >= 3 :
+        conj = conj[0:3]
+    move, value = PVS_root(depth, board, conj, t0)
+    return move, value
+
+class ThreadWithReturnValue(Thread):
+    def __init__(self, group=None, target=None, name=None,
+                 args=(), kwargs={}, Verbose=None):
+        Thread.__init__(self, group, target, name, args, kwargs)
+        self._return = None
+    def run(self):
+        #print(type(self._target))
+        if self._target is not None:
+            self._return = self._target(*self._args,
+                                                **self._kwargs)
+    def join(self, *args):
+        Thread.join(self, *args)
+        return self._return
+
+def thread(board: chess.Board, depth: int) :
+    values = []
+    moves = []
+    for move in board.legal_moves :
+        pos = deepcopy(board)
+        pos.push(move)
+        moves.append(str(move))
+        values.append(ThreadWithReturnValue(target=pvSearch, args=(pos, -float('inf'), float('inf'), depth-2)))
+    for _thread in values :
+        _thread.start()
+    for i in range(len(values)) :
+        values[i] = values[i].join()
+    if board.turn :
+        value = -min(values)
+    else :
+        value = min(values)
+    print(value)
+    return moves[values.index(min(values))], value
